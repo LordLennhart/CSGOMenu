b4e3394c6d1f36fa5ef5ad3b28a79e6d11b83395
LordLennhart <lennart.schroeder007@gmail.com>
2021-11-09 20:14:06 +0100

    DLL ATTACH

--- CSGOMenu/dllmain.cpp
+++ CSGOMenu/dllmain.cpp
@@ -3,10 +3,34 @@
 
 #include "Aimbot.h"
 
+DWORD WINAPI OnDllAttach(PVOID base) {
+  while (!GetAsyncKeyState(VK_DELETE) & 0x8000) {
+    Run();
+    Sleep(1);
+  }
+  FreeLibraryAndExitThread(static_cast<HMODULE>(base), 0);
+}
+
+VOID WINAPI OnDllDetach() {
+//#ifdef _DEBUG
+//  HWND hw_ConsoleHwnd = GetConsoleWindow();
+//  PostMessageW(hw_ConsoleHwnd, WM_CLOSE, 0, 0);
+//#endif  // _DEBUG
+}
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call,
                       LPVOID lpReserved) {
-  switch (ul_reason_for_call) {
+  if (ul_reason_for_call = DLL_PROCESS_ATTACH) {
+    DisableThreadLibraryCalls(hModule);
+    CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)OnDllAttach, hModule, NULL,
+                 NULL);
+  } else if (ul_reason_for_call = DLL_PROCESS_DETACH) {
+    OnDllDetach();
+  }
+
+  /* switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH:
+
       __fallthrough;
     case DLL_PROCESS_DETACH:
       MessageBeep(MB_OK);
@@ -18,6 +42,6 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call,
       MessageBeep(MB_OK);
       break;
   }
-  Beep(100, 2000);
+  Beep(100, 2000); */
   return TRUE;
 }
