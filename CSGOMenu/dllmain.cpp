9db424468146da70928831c0678c87a8e4632a23
LordLennhart <lennart.schroeder007@gmail.com>
2021-11-09 20:25:06 +0100

    ATTACK FIX?

--- CSGOMenu/dllmain.cpp
+++ CSGOMenu/dllmain.cpp
@@ -7,6 +7,13 @@
 #include "Aimbot.h"
 
 DWORD WINAPI OnDllAttach(PVOID base) {
+#ifdef _DEBUG
+  AllocConsole();
+  freopen_s((FILE**)stdin, "CONIN$", "r", stdin);
+  freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
+  SetConsoleTitleA("BLA - Debug");
+#endif  // _DEBUG
+
   while (!GetAsyncKeyState(VK_DELETE) & 0x8000) {
     Run();
     Sleep(1);
@@ -15,19 +22,23 @@ DWORD WINAPI OnDllAttach(PVOID base) {
 }
 
 VOID WINAPI OnDllDetach() {
-  //#ifdef _DEBUG
-  //  HWND hw_ConsoleHwnd = GetConsoleWindow();
-  //  PostMessageW(hw_ConsoleHwnd, WM_CLOSE, 0, 0);
-  //#endif  // _DEBUG
+#ifdef _DEBUG
+  fclose((FILE*)stdin);
+  fclose((FILE*)stdout);
+
+  HWND hw_ConsoleHwnd = GetConsoleWindow();
+  FreeConsole();
+  PostMessageW(hw_ConsoleHwnd, WM_CLOSE, 0, 0);
+#endif  // _DEBUG
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call,
                       LPVOID lpReserved) {
-  if (ul_reason_for_call = DLL_PROCESS_ATTACH) {
+  if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
     DisableThreadLibraryCalls(hModule);
     CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)OnDllAttach, hModule, NULL,
                  NULL);
-  } else if (ul_reason_for_call = DLL_PROCESS_DETACH) {
+  } else if (ul_reason_for_call == DLL_PROCESS_DETACH) {
     OnDllDetach();
   }
 
